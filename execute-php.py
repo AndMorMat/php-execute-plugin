b6f48b47ee767216e8baee360a8388883bd625c0
andre <andrematos.ti@procob.com>
2020-11-12 14:43:55 -0300

    Adicionando script e imagem de exemplo

---
+++ execute-php.py
@@ -0,0 +1,31 @@
+import sublime
+import sublime_plugin
+
+import subprocess
+
+class ExecuteCommand(sublime_plugin.TextCommand): 
+	
+	file_dir = '/tmp/execute.php';
+	php_path = '/usr/bin/php';
+
+	def run(self, edit):
+		stri = '';
+		for sel in self.view.sel():
+			line = self.view.line(sel) 
+			stri += str(self.view.substr(line).strip())
+
+		arquivo = open(self.file_dir, 'w')
+		inicio = stri[:5]
+		if inicio != "<?php":
+			stri = "<?php "+stri
+
+
+		arquivo.write(stri)
+		arquivo.close();
+
+		cmd = [self.php_path] + [self.file_dir]
+
+		p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		out, err = p.communicate()
+
+		self.view.insert(edit, line.end(), '\n/* {0} */'.format((out if not err else err).decode().strip()))
